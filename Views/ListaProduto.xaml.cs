3b540f2436c103311a83798a5c81bf519c805264
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2025-03-16 15:37:43 -0300

    add delete products feat

--- Views/ListaProduto.xaml.cs
+++ Views/ListaProduto.xaml.cs
@@ -55,5 +55,27 @@ public partial class ListaProduto : ContentPage
         DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
-    
+    private async void Remove_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            MenuItem selecinado = sender as MenuItem;
+
+            Produto p = selecinado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert(
+                "Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
+            {
+                await((App)Application.Current).Db.Delete(p.Id);
+                lista.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
 }
